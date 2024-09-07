ae1ca3f308653cd99f7d93c61ffe21a820dd5241
Itay <itay.responder@gmail.com>
2024-09-07 19:40:42 +0300

    after build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: '../doneday-backend/public',
+        outDir: '../AvivaDay-backend/public',
         emptyOutDir: true,
     },
     define: {
